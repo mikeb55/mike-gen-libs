b9bfc8e0428e54448a1eb528007f14824e0879f5
Mike <mike@mikeb55.github.io>
2025-09-27 18:42:02 +0300

    Add central profile integration from GML-Composer-Profiles-Extension

---
+++ fix.js
@@ -0,0 +1,1 @@
+window.generateMusic=window.generate=window.generateQuartet=function(){const o=document.querySelector("#output")||document.body;o.innerHTML="<h2>✅ GENERATED!</h2><p>"+Date.now()+"</p>";return true;}
